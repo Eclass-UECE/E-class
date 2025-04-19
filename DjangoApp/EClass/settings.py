f46ecf58fa0cfa2eca230e3728335b91f4e3ed72
João Pedro Araújo <joaoao.0175@gmail.com>
2025-04-19 15:14:12 -0300

    Minor chances

--- DjangoApp/EClass/settings.py
+++ DjangoApp/EClass/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'NAME': 'E-Class',
         'USER': 'postgres',
         'PASSWORD': 'JONICAGE123',
-        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
-        'PORT': '',          # Leave empty to use the default PostgreSQL port (usually 5432)
+        'HOST': 'localhost',  
+        'PORT': '5432',          
     }
 }
 
